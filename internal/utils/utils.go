8c17ba47d14d2ae1f7e0a8cf103acfd00cfacdc7
HMXyber <33129876+HMXyber@users.noreply.github.com>
2021-08-09 09:56:33 +0300

    Feature/task 2 (#2)
    * lesson-2
    
    * add error checking

---
+++ internal/utils/utils.go
@@ -0,0 +1,71 @@
+package utils
+
+import "errors"
+
+func Split(source []int, chunkSize int) ([][]int, error) {
+	if chunkSize < 1 {
+		return nil, errors.New("chunkSize should be greater then 1")
+	}
+	if source == nil {
+		return nil, errors.New("source slice is nil")
+	}
+	chunkCount := (len(source) + chunkSize - 1) / chunkSize
+	result := make([][]int, chunkCount)
+	var rightPosition, index int
+
+	for leftPosition := 0; leftPosition < len(source); leftPosition += chunkSize {
+		rightPosition = min(leftPosition+chunkSize, len(source))
+
+		batch := source[leftPosition:rightPosition]
+		result[index] = batch
+		index++
+	}
+
+	return result, nil
+}
+
+func SwapKeyValues(source map[string]string) (map[string]string, error) {
+	if source == nil {
+		return nil, errors.New("source is nil")
+	}
+	resultMap := make(map[string]string, len(source))
+
+	for k, v := range source {
+		resultMap[v] = k
+	}
+	return resultMap, nil
+}
+
+func Disjoin(source []string, filterCollection []string) ([]string, error) {
+	if source == nil {
+		return nil, errors.New("source slice is nil")
+	}
+	var result []string
+	cachedFilter := getMapFromSlice(filterCollection)
+
+	for i := 0; i < len(source); i++ {
+		if _, ok := cachedFilter[source[i]]; ok {
+			continue
+		}
+		result = append(result, source[i])
+	}
+	return result, nil
+}
+
+func getMapFromSlice(source []string) map[string]bool {
+	if source == nil {
+		return map[string]bool{}
+	}
+	result := make(map[string]bool, len(source))
+	for i := 0; i < len(source); i++ {
+		result[source[i]] = true
+	}
+	return result
+}
+
+func min(a int, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
