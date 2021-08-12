0b671a38047d0464c6ae452b82053b972b71a1ac
xyber <xyber@mail.ru>
2021-08-12 21:08:09 +0300

    task-3

---
+++ internal/domain/eventTime.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type EventTime struct {
+	time.Time
+}
+
+func (et EventTime) String() string {
+	bytes, err := et.MarshalJSON()
+	if err != nil {
+		fmt.Println(err)
+		return et.String()
+	}
+	return string(bytes)
+}
+
+const layout = "2006-01-02 15:04:05"
+
+func (et *EventTime) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), `"`)
+	if s == "null" {
+		return
+	}
+	et.Time, err = time.Parse(layout, s)
+	return
+}
+
+func (et EventTime) MarshalJSON() ([]byte, error) {
+	if et.Time.IsZero() {
+		return nil, nil
+	}
+	return []byte(fmt.Sprintf(`"%s"`, et.Time.Format(layout))), nil
+}
